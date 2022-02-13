9574fdefdd5c98594c148c16bfc68829944598e0
Deodat Kimuene <dkimuene1@gmail.com>
2022-02-13 12:15:50 -0600

    Delete Items
    We can now delete items from the item list, This is done by clicking on the list item where a pop-up will confirm if you want to remove the item.

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
 //This is a function that hold all functions and event listeners 
+const listArr = [];
 function init () {
     // This is the text field where you write your to dos
     const input = document.getElementById("item-name");
@@ -6,6 +7,7 @@ function init () {
     const button = document.getElementById('button');
     //This is container the to do items will be put in to
     const list = document.querySelector('.item-container');
+    
 // This is a function that will get the to do item enter into the text field
 function getValue(){
     return val = input.value
@@ -13,13 +15,29 @@ function getValue(){
 /* This is an event listener that looks for clicks on the submit buttton, it makes a new list item, takes the name that was enter, 
 adds the name to the list item , adds the class of item for style, adds the list item to the list*/
 button.addEventListener("click", (event) => {
+    event.preventDefault();
     let item = document.createElement('li');
     let itemName = document.createTextNode(getValue());
     item.appendChild(itemName);
     item.className ="item";
     list.appendChild(item);
-    event.preventDefault();
-    });    
+
+    item.addEventListener("click", (event) => {
+        let answer = confirm("Would you like to remove this item?");
+        if(answer){
+            list.removeChild(item)
+        }
+    });
+    
+    });  
 }
 //This will make sure we wait until everything is load on the page
 window.addEventListener("load", init);
+
+let listItem = document.querySelector(".item");
+
+console.log(listArr)
+// listItem.addEventListener('click', (event) => {
+//     console.log("Hello");
+    
+// });
